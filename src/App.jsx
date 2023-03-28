7806479ca698c634eafd104ed01424b691dde94e
JH9001 <josh.fsd@icloud.com>
2023-03-28 19:35:22 +1100

    update page names, pokedex to home, pokemon to about, update page imports in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Pokedex from "./pages/Pokedex";
-import Pokemon from "./pages/Pokemon";
+import Home from "./pages/Home";
+import About from "./pages/About";
 import Error from "./pages/Error";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Pokedex />} />
-        <Route path="pokemon/:id" element={<Pokemon />} />
+        <Route path="/" element={<Home />} />
+        <Route path="pokemon/:id" element={<About />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
